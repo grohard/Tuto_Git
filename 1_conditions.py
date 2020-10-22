8cbdb136f815c9610b825bf5fb9b7b272c5cde51
Guillaume ROHARD <rohard.guillaume@wanadoo.fr>
2020-10-22 11:44:04 +0200

    changement1

---
+++ 1_conditions.py
@@ -0,0 +1,13 @@
+age=input("Quel âge avez-vous ? ")
+if int(age) < 18:
+    prix = 7
+    print("Prix : "+str(prix)+"€")
+else:
+    prix = 12
+    print("Prix: "+str(prix)+"€")
+
+pop_corn = input("Voulez vous des pop-corns ? (o/n)")
+
+new_prix = (prix,prix+5)[pop_corn=="o"] #condition ternaire
+
+print ("Cela vous fera un total de {} €".format(new_prix))
